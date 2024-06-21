a2d5821b900a4ad3d6fcb2378c122d70f91705db
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-06-21 05:54:35 -0700

    1.5 Update Support

--- commands/support.js
+++ commands/support.js
@@ -29,10 +29,9 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/XeekQ4mjrE";
+      const githubLink = "https://github.com/itsb0ss";
+      const youtubeLink = "https://youtube.com/@_itsb0ss";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -40,8 +39,8 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- YouTube - ${youtubeLink}`)
+            .setImage('https://media.discordapp.net/attachments/1250009420931403810/1253694445963182120/kendrick2.png?ex=6676c970&is=667577f0&hm=0e01636f08080d8bb591ff763203979ea57428114432f201f9ab4582708941df&=&format=webp&quality=lossless&width=550&height=261')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
