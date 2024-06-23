436dbcab6b8d593b7ebd8817d11e4d0251ea3155
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-06-23 06:28:59 -0700

    1.6.1 Alternative Lavalink
    Changed lavalink to a thirs alternative host.

--- player.js
+++ player.js
@@ -5,9 +5,9 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
         {
-            host: "37.114.42.191",
-            port: 6767,
-            password: "danteisnttaken",
+            host: "lavalink.oryzen.xyz",
+            port: 80,
+            password: "oryzen.xyz",
             secure: false
         },
     ];
