a0aaab2b56ec880c907a9a2815f80ee8430158b4
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-10-14 23:38:27 -0700

    1.7 - Update config.js

--- config.js
+++ config.js
@@ -19,16 +19,34 @@
 
 */
 
-
-
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["636615832218239016", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "00fbff",
+  embedColor: "#1db954",
+  musicardTheme:"themes16", //Goes from themes1 to themes19
+  activityName: "You", // This is bot status Write Anything here 
+  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/uklnrp",
+  CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+  MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+  embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
 
+   // Lavalink Server Details
+
+  nodes: [
+    {
+        name: "Server",
+        host: "nodev4.fypmoon.org",
+        port: 1118,
+        password: "fypmoon.org",
+        reconnectTimeout: 5000,
+        reconnectTries: Infinity,
+        secure: false
+    },
+ ]
 }
 /*
 
@@ -50,4 +68,3 @@ module.exports = {
 
 
 */
-
