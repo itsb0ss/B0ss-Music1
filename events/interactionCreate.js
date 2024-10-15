0931b6a2d2026b25ae25165995027ae548cdb245
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-10-15 01:30:30 -0700

    1.8 - Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -21,41 +21,42 @@
 
 
 const config = require("../config.js");
-const { ActivityType } = require("discord.js");
+const { InteractionType } = require('discord.js');
+const fs = require("fs");
 
-module.exports = async (client) => {
-    const { REST } = require("@discordjs/rest");
-    const { Routes } = require("discord-api-types/v10");
-    const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-
-    (async () => {
-        try {
-            await rest.put(Routes.applicationCommands(client.user.id), {
-                body: await client.commands,
+module.exports = async (client, interaction) => {
+  try {
+    if (!interaction?.guild) {
+      return interaction?.reply({ content: "This command can only be used in a server.", ephemeral: true });
+    } else {
+      function cmd_loader() {
+        if (interaction?.type === InteractionType.ApplicationCommand) {
+          fs.readdir(config.commandsDir, (err, files) => {
+            if (err) throw err;
+            files.forEach(async (f) => {
+              let props = require(`.${config.commandsDir}/${f}`);
+              if (interaction.commandName === props.name) {
+                try {
+                  if (interaction?.member?.permissions?.has(props?.permissions || "0x0000000000000800")) {
+                    return props.run(client, interaction);
+                  } else {
+                    return interaction?.reply({ content: `You don't have permission to use this command.`, ephemeral: true });
+                  }
+                } catch (e) {
+                  return interaction?.reply({ content: `❌ Error: ${e.message}`, ephemeral: true });
+                }
+              }
             });
-            console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
-        } catch (err) {
-            console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
+          });
         }
-    })();
+      }
 
-    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
-
-    const activityType = ActivityType[config.activityType.charAt(0).toUpperCase() + config.activityType.slice(1).toLowerCase()];
-    if (!activityType) {
-        console.error(`Invalid activity type: ${config.activityType}`);
-        return;
+      cmd_loader();
     }
-    
-    setInterval(() => client.user.setActivity({ 
-        name: config.activityName, 
-        type: activityType 
-    }), 10000);
-
-    client.errorLog = config.errorLog;
-};
-
-
+  } catch (e) {
+    console.error(e);
+  }
+}
 /*
 
   ________.__                        _____.___.___________
