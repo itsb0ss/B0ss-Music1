401970111c1c68f081c5423b53735389c4f80ee2
Boss <161413826+itsb0ss@users.noreply.github.com>
2024-11-03 23:03:00 -0800

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        name: "Working2",
+        host: "lavalink.clxud.dev",
+        port: 2333,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
